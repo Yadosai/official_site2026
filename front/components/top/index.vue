668c8901561c21415c8581bc592c16ef3b0b678e
杉本光輝 <suumii.0999@outlook.com>
2026-03-20 01:11:28 +0900

    Fix:sentences

--- front/components/top/index.vue
+++ front/components/top/index.vue
@@ -4,15 +4,9 @@
         <div class="text-overlay">
             <p class="eyeCatchTitle">やどかり祭</p>
             <p class="eyeCatchSubtitle">～<ruby>祭開始<rt>再スタート</rt></ruby>～</p>
-            <p class="eyeCatchDate">5/30,6/1</p>
+            <p class="eyeCatchDate">5/29,5/30</p>
         </div>
         
-    </div>
-    <div id="updateInfo">
-        <p class="updateInfoTitle">ご報告</p>
-        <div class="updateInfoContent">
-            <updataInfoItem updateDate="" contentText="このたび、5月30日(金)の宿舎祭前夜祭にて、社会工学類1年2・4クラスの模擬店「クレープ&スムージークレイジー」において、ブラスチック片が混入したスムージー約8食のうち、4食が提供されていたことが判明しました。20時頃に委員が状況を確認し、ただちに販売の中止を命じました。該当商品はすべて廃棄するよう指示し、出店団体には厳重注意を行いました。6月1日(日)の本祭において同団体からはスムージーの提供は行いませんでした。また、提供されたスムージーのうち2食は、すでに委員会で回収済みです。現在のところ健康被害の報告はございませんが、体調に不安がある方はyadosai.tkb@gmail.com までご連絡ください。各種SNSのDMでも対応しております。" />
-        </div>
     </div>
     <div id="LinkButtons">
         <buttonItem textHighlightedFirst="模" textMiddlePart="擬店" colorType="black" position="left"
